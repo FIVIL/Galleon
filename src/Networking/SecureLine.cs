63a7c14ef9f85082e2bd763f8eb71b6dbb06bc9c
FIVIL <31272779+FIVIL@users.noreply.github.com>
2018-05-13 11:48:52 +0430

    nc- networking adding line manager system, in which line sends data raw and secure line stablishes a secure line using aes and key encryption with rsa and then send and recive data securly.

---
+++ src/Networking/SecureLine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galleon.Networking
+{
+    class SecureLine : ILine
+    {
+        public Line ConnectionLine { get; private set; }
+    }
+}
